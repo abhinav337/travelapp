1c0ecd91b45c0e64ae8406ad260f76e84dd01b7e
abhinav337 <77365702+abhinav337@users.noreply.github.com>
2021-03-13 15:11:38 +0900

    Update app.js

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -53,7 +53,6 @@ export const sendServer = async (url = '', data = {}) => {
     })
     try {
         const completeInfo = await req.json();
-        //console.log("completeInfo: ", completeInfo);
         return completeInfo;
     } catch (error) {
         console.log("error", error);
@@ -74,7 +73,6 @@ export function submitInputs(e) {
     geoNames(toCity)
       .then((cityInfo) => {
         const country = cityInfo.geonames[0].countryName;
-        console.log(country);
         const weatherInfo = weatherBit(toCity, daysLeft);
         return weatherInfo;
       })
@@ -87,7 +85,6 @@ export function submitInputs(e) {
               minTemp: daysLeft< 16 ? weatherInfo.min_temp : "N/A", 
               summary: daysLeft < 16 ? weatherInfo.weather.description : null
         });
-        console.log(completeInfo);
         return completeInfo;
       }).then((completeInfo) => {
           modifyUI(completeInfo);
@@ -99,10 +96,7 @@ export const modifyUI = async (completeInfo) => {
     document.querySelector('#result').classList.remove("hidden");
     const toCity = document.querySelector("#tocity").value;
     const startDate = document.querySelector("#startdate").value;
-    //const cityUrl = `https://pixabay.com/api/?key=19873002-2745dade829779eccdc1a2cfe&q=${toCity}&image_type=photo&pretty=true&category=places`;
-    //const res = await fetch(cityUrl);
     try {
-        //const image = await res.json();
         const imageURL = pixaBayJson.hits[0].webformatURL;
         document.querySelector("#cityimage").setAttribute('src', imageURL);
         document.querySelector("#travelcity").innerHTML = completeInfo.city;
